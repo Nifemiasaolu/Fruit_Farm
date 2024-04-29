2eac76af1d44ab17f9a4e75044ec74812d1570e5
nifemiasaolu <theophilusasaolu@gmail.com>
2024-04-29 05:50:47 -0700

    Details updated

---
+++ index.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+/////////////////////////////////////////////////////////////
+//  FILES
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!')
+
+// Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if(err) return(console.log("ERROR💣🎇🔥"))
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('File has been written🥰😁')
+//       })
+//     });
+//   });
+// });
+
+// console.log("Will read file");
+
+/////////////////////////////////////////////////////////////
+//  SERVER
+try{
+
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+
+  const {query, pathname} =(url.parse(req.url, true))
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(tempOverview);
+
+    // Product page
+  } else if (pathname === "/product") {
+    res.end(tempProduct);
+
+    // API
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    // NOT_FOUND
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page Not Found!</h1>");
+  }
+  res.end("Hello fron the Server World!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
+} catch(error) {
+  console.log("Error reading file: ", error);
+}
+//\\\\\\\\\
